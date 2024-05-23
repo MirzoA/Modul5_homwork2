9a59f5e146f37040231940e5508b3d034a231141
MirzoA <102673274+MirzoA@users.noreply.github.com>
2024-05-23 20:18:22 +0300

    Add files via upload

--- Modul_5_homework_2_Class_House.py
+++ Modul_5_homework_2_Class_House.py
@@ -4,11 +4,11 @@ class House:  # создание класса
         # (self, number_of_floors=0) - параметры функции
         self.floors = number_of_floors  # атрибут экземпляра класса
 
-    def setNewNumberOfFloors(self, floor=1):  # метод класса
+    def set_new_number_of_floors(self, floor=1):  # метод класса
         self.floors += 1
         print(self.floors)
 
 
 house1 = House()  # создание экземпляра
 
-house1.setNewNumberOfFloors()
+house1.set_new_number_of_floors()
